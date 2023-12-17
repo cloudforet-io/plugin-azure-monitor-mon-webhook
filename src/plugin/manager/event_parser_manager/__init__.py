5e4659e4a20f4f7da08e6451ce857be2b8b2ff18
ImMin5 <mino@megazone.com>
2023-12-17 13:52:54 +0900

    feat: add AzureMonitorMetricAlert schema manager

--- src/plugin/manager/event_parser_manager/__init__.py
+++ src/plugin/manager/event_parser_manager/__init__.py
@@ -1,3 +1,6 @@
 from plugin.manager.event_parser_manager.commom_alert_manager.monitor_alert_schema_manager import (
     MonitorAlertSchemaManager,
 )
+from plugin.manager.event_parser_manager.non_common_alert_manager.monitor_metric_alert_manager import (
+    MonitorMetricAlertManager,
+)
